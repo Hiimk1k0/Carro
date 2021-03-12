3d82f9497e4d1d5ef765e2044ffd431d97c22659
Hiimk1k0 <fmfsmgaio@gmail.com>
2021-03-12 16:11:03 +0000

    Versão 2 da Classe Carro com JavaDoc a descrever a classe

--- src/Carro.java
+++ src/Carro.java
@@ -1,4 +1,22 @@
 
+/**
+ * A classe carro contem metodos para realizar funcionalidades basicas de um carro
+ * 
+ * <ul>
+ * <li>getConsumo</li>
+ * <li>getQuantidadeCombustivel</li>
+ * <li>getCapacidadeDeposito</li>
+ * <li>meterCombustivel</li>
+ * <li>meterCombustivel</li>
+ * <li>andar</li>
+ * <li>parar</li>
+ * <li>obterAutonomia</li>
+ * </ul>
+ * 
+ * @author Hiimk1k0
+ *
+ */
+
 public class Carro {
 	
 	static private final double consumo=5.5; // litros por 100 klm
@@ -16,28 +34,59 @@ public class Carro {
 	
 	// Acessores 
 
+	/**
+	 * Consumo
+	 * 
+	 * @return retorna o consumo
+	 */
+	
 	public static double getConsumo() {
 		return consumo;
 	}
 
+	/**
+	 * Quantidade de combustivel
+	 * 
+	 * @return retorna a quantidade de Combustivel
+	 */
+	
 	public double getQuantidadeCombustivel() {
 		return quantidadeCombustivel;
 	}
 
+	/**
+	 * Informa sobre a capacidade do deposito
+	 * 
+	 * @return retorna a capacidade do Deposito do carro
+	 */
+	
 	public double getCapacidadeDeposito() {
 		return capacidadeDeposito;
 	}
 	
 	// Métodos 
 	
-	public void meterCombustivel( double quantidade) { 		
+	/**
+	 * Testa o combustivel para ver se nao transborda
+	 * 
+	 * @param quantidade valor da quantidade metida no carro
+	 * @return quantidade de litros metidos
+	 */
+	
+	public double meterCombustivel( double quantidade) { 		
 		
 		// Devia testar se Transborda o depósito
 		// Podia retornar os Litros Metidos 
 		quantidadeCombustivel = quantidadeCombustivel + quantidade;
 		
-		
+		return quantidade;
 	}
+	
+	/**
+	 * Atesta o deposito
+	 * 
+	 * @return retorna os litros Meter
+	 */
 	
 	// atestar o depósito
 	public double meterCombustivel() {
@@ -50,6 +99,12 @@ public class Carro {
 		return litrosMeter;
 	}
 	
+	/**
+	 * Faz o carro andar
+	 * 
+	 * @param distancia valor da distancia a percorrer
+	 * @return true se o carro consegue percorrer a distancia e false se nao consegue
+	 */
 	
 	// distancia em klm
 	public boolean andar( double distancia ) {
@@ -70,12 +125,24 @@ public class Carro {
 			
 	}
 	
+	/**
+	 * Faz o carro parar
+	 * 
+	 * @return true para o carro parar
+	 */
+	
 	public boolean parar() {
 		
 		// se estiver a andar pára 
 		// falta a flag que indica se o carro está em andamento
 		return true;
 	}
+	
+	/**
+	 * Informa da autonomia do carro
+	 * 
+	 * @return a autonimia do carro
+	 */
 	
 	// kmCombustivel( )    
 	public double obterAutonomia() {
